3f1c77d4564eed41a57152c4e67eb3aa79a06e91
Khanh ngu yen <khanhntqgcd210526@fpt.edu.vn>
2024-03-03 16:35:22 +0700

    Edit User information, Reset User Password, Notification

--- UniMagazine/Areas/Admin/Controllers/UserController.cs
+++ UniMagazine/Areas/Admin/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using UniMagazine.Repository.IRepository;
 using UniMagazine.Models.ViewModels;
 using UniMagazine.Models;
 using System.Data;
+using Microsoft.DiaSymReader;
 
 namespace UniMagazine.Areas.Admin.Controllers
 {
@@ -27,8 +28,9 @@ namespace UniMagazine.Areas.Admin.Controllers
         public IActionResult UserIndex(string search = "", string role = "", int facultyId = 0)
         {
             IEnumerable<ApplicationUser> users = _unitOfWork.UserRepository.GetAllUser(search, role, facultyId);
-            
-            UserVM userVM = new UserVM() { 
+
+            UserVM userVM = new UserVM()
+            {
                 Users = users,
                 SearchByEmail = search,
                 Role = role,
@@ -55,14 +57,14 @@ namespace UniMagazine.Areas.Admin.Controllers
                     Value = r.Name
                 })
             };
-           
+
             return View(rm);
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateAccount(RegisterModel rm)
         {
-            
+
             if (ModelState.IsValid && await _userManager.FindByEmailAsync(rm.Email) == null)
             {
                 var user = new ApplicationUser()
@@ -99,6 +101,108 @@ namespace UniMagazine.Areas.Admin.Controllers
                 });
             }
             return View(rm);
+        }
+
+        public async Task<IActionResult> UpdateUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            UpdateUserModel um = new UpdateUserModel()
+            {
+                Id = user.Id,
+                FullName = user.FullName,
+                Email = user.Email,
+                Role = user.Role,
+                DateOfBirth = user.DateOfBirth,
+                Address = user.Address,
+                FacultyId = user.FacultyId,
+                Faculty = _unitOfWork.FacultyRepository.Get(user.FacultyId),
+
+                Faculties = _unitOfWork.FacultyRepository.GetAllFaculty().Select(f => new SelectListItem
+                {
+                    Text = f.Name,
+                    Value = f.Id.ToString()
+                }),
+
+                Roles = _roleManager.Roles.Select(r => new SelectListItem
+                {
+                    Text = r.Name,
+                    Value = r.Name
+                })
+            };
+
+
+            return View(um);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateUser(UpdateUserModel um)
+        {
+            var user = await _userManager.FindByIdAsync(um.Id);
+            if (user == null) return NotFound();
+
+            user.FullName = um.FullName;
+            user.Address = um.Address;
+            user.DateOfBirth = um.DateOfBirth;
+            user.FacultyId = um.FacultyId;
+
+            if (user.Role != um.Role)
+            {
+                user.Role = um.Role;
+                await _userManager.AddToRoleAsync(user, user.Role);
+            }
+
+            await _userManager.UpdateAsync(user);
+            return RedirectToAction("UserIndex");
+        }
+
+        public async Task<IActionResult> ResetPassword(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            ResetPasswordVM rvm = new ResetPasswordVM()
+            {
+                Email = user.Email,
+                Password = user.PasswordHash,
+                ConfirmPassword = user.PasswordHash
+            };
+            return View(rvm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordVM rvm)
+        {
+
+            var email = Request.Form["email"];
+            var password = Request.Form["password"];
+            var user = await _userManager.FindByEmailAsync(email);
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            if (user == null)
+                return NotFound();
+            var result = await _userManager.ResetPasswordAsync(user, code, password);
+            if (result.Succeeded)
+            {
+                TempData["success"] = $"Password reset successfully! for (Email:  {user.Email})";
+                return RedirectToAction("UserIndex");
+            }
+
+            //error
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            rvm = new ResetPasswordVM()
+            {
+                Email = user.Email,
+                Password = user.PasswordHash,
+                ConfirmPassword = user.PasswordHash,
+                Code = user.Id
+            };
+            TempData["error"] = "Something wrong!";
+            return View(rvm);
         }
     }
 }
---
+++ UniMagazine/Models/ResetPasswordVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniMagazine.Models
+{
+    public class ResetPasswordVM
+    {
+        [Required]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password cannot be empty !!")]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,100}$", ErrorMessage = "The {0} must contain at least one letter, one number, and one special character.")]
+        [DataType(DataType.Password)]
+        public string? Password { set; get; }
+
+
+        [Required(ErrorMessage = "Confirm password cannot be empty !!")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string? ConfirmPassword { set; get; }
+        
+        [Required]
+        public string? Code { get; set; }
+    }
+}
