8dfe4a7521cf04be06ca3c6af982b376ee4fee1b
Khanh ngu yen <khanhntqgcd210526@fpt.edu.vn>
2024-03-20 22:41:31 +0700

    Migration of MaterialContribution

---
+++ UniMagazine/Migrations/20240319154249_add_materialCon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniMagazine.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_materialCon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MaterialContributions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaterialContributions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MaterialContributions");
+        }
+    }
+}
