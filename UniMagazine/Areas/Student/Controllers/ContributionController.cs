bf13c957a529903ae01461e68e91e650c4e2a241
Khanh ngu yen <khanhntqgcd210526@fpt.edu.vn>
2024-03-31 17:28:30 +0700

    Fix the location of file for student contribution ()

--- UniMagazine/Areas/Student/Controllers/ContributionController.cs
+++ UniMagazine/Areas/Student/Controllers/ContributionController.cs
@@ -94,7 +94,7 @@ namespace UniMagazine.Areas.Student.Controllers
                             var MaCon = new MaterialContribution()
                             {
                                 CreatedDate = DateTime.Now,
-                                ImageUrl = filePath + fileName,
+                                ImageUrl = @"upload\Student\" + fileName,
                                 ContributionId = conTri.Id // Set ContributionId with the generated Id of Contribution entity
                             };
                             _unitOfWork.MaterialContributionRepository.Add(MaCon);
