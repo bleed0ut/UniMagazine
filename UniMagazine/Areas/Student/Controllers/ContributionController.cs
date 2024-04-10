c830de0ff424061faf87d6a99cc8698aeeb65f92
phuc <phucpvp7@gmail.com>
2024-04-10 09:22:22 +0700

    merge_dash

--- UniMagazine/Areas/Student/Controllers/ContributionController.cs
+++ UniMagazine/Areas/Student/Controllers/ContributionController.cs
@@ -101,7 +101,7 @@ namespace UniMagazine.Areas.Student.Controllers
                             {
                                 CreatedDate = DateTime.Now,
                                 ImageUrl = @"\upload\Student\" + fileName,
-                                ContributionId = conTri.Id // Set ContributionId with the generated Id of Contribution entity
+                                ContributionId = con.Id // Set ContributionId with the generated Id of Contribution entity
                             };
                             _unitOfWork.MaterialContributionRepository.Add(MaCon);
                         }
